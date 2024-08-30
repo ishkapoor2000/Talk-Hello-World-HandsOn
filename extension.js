4becc5fa015f59b9153980c7f89660fe2eab91bb
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2024-08-30 18:49:24 +0530

    ''

--- extension.js
+++ extension.js
@@ -2,8 +2,8 @@ const vscode = require('vscode');
 const { createClient } = require('@supabase/supabase-js');
 
 function activate(context) {
-    const SUPABASE_URL = 'https://ovdibwrrpuxsdzfqfeft.supabase.co';
-    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92ZGlid3JycHV4c2R6ZnFmZWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NjkwNTQsImV4cCI6MjA0MDU0NTA1NH0.TgW1xWBGdlFhqa400rl3HSmxPVBonUpgsQYFVpcVITs';
+    const SUPABASE_URL = '';
+    const SUPABASE_KEY = '';
     const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
     let disposable = vscode.commands.registerCommand('hello-world-supabase-vscode-extension.storeRandomNumber', async function () {
